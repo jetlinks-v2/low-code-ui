40bdde7acbe228f1d2c3d641245051bb80e27dd5
jackhoo_98 <jackhoo_98@foxmail.com>
2023-08-09 21:40:35 +0800

    feat: 快速编辑表格组件 input编辑基本功能

---
+++ apps/Admin/src/components/QuickEditTable/components/EditInput/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <input
+    type="text"
+    class="edit-input"
+    :value="value"
+    @input="onInput"
+    @blur="onBlur"
+    @click="onClick"
+  />
+</template>
+
+<script setup lang="ts" name="EditInput">
+import { ref } from 'vue'
+const props = defineProps({
+  value: {
+    type: String,
+    default: '',
+  },
+  setValue: {
+    type: Function,
+    default: () => {},
+  },
+})
+
+
+const inputValue = ref(props.value)
+
+const onInput = (e: Event) => {
+  inputValue.value = (e.target as HTMLInputElement).value.trim()
+}
+
+const onClick = (e: Event) => {
+  e.stopPropagation()
+}
+
+const onBlur = () => {
+  props.setValue(inputValue.value)
+}
+</script>
+
+<style lang="less" scoped>
+.edit-input {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  border: 1px solid orange;
+}
+</style>
--- apps/Admin/src/views/center/index.vue
+++ apps/Admin/src/views/center/index.vue
@@ -1,12 +1,91 @@
 <template>
   <div>
-    <quick-edit-table></quick-edit-table>
+    <quick-edit-table
+      :data="data"
+      :columns="columns"
+      @submit="handleSubmit"
+    ></quick-edit-table>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'index',
+<script setup lang="ts">
+import { ref, watch, toRefs, onMounted } from 'vue'
+
+const columns = [
+  { title: '序号', key: 'index', editable: false },
+  {
+    title: '列',
+    key: 'column',
+    description: '蛇形命名',
+    iconType: 'QuestionCircleOutlined',
+    editable: false,
+  },
+  { title: '别名', key: 'alias', description: '驼峰命名', editable: false },
+  { title: '注释', key: 'comment', editable: false },
+  { title: 'jdbcType', key: 'jdbcType', editable: true },
+  { title: 'javaType', key: 'javaType', editable: true },
+  { title: '长度', key: 'length', editable: true },
+  { title: '精度', key: 'precision', editable: false },
+  { title: '只读', key: 'readOnly', editable: true },
+  { title: '其他配置', key: 'otherConfig', editable: true },
+  { title: '操作', key: 'action', editable: true },
+]
+
+// const data = ref([
+//   {
+//     column: '列数据',
+//     alias: '别名数据',
+//     comment: '注释数据',
+//     jdbcType: 'jdbcType数据',
+//     javaType: 'javaType数据',
+//     length: '长度数据',
+//     precision: '精度数据',
+//     readOnly: '只读数据',
+//     otherConfig: '其他配置数据',
+//     action: '操作数据',
+//     key: 1,
+//     index: 1,
+//   },
+// ])
+
+const data = ref([])
+// 模拟生成大量数据
+for (let i = 1; i <= 10000; i++) {
+  data.value.push({
+    column: '列数据',
+    alias: '别名数据',
+    comment: '注释数据',
+    jdbcType: 'jdbcType数据',
+    javaType: 'javaType数据',
+    length: '长度数据',
+    precision: '精度数据',
+    readOnly: '只读数据',
+    otherConfig: '其他配置数据',
+    action: '操作数据',
+    key: i,
+    index: i,
+    // column: '列数据',
+    // alias: '别名数据',
+    // comment: '注释数据',
+    // jdbcType: 'jdbcType数据',
+    // javaType: 'javaType数据',
+    // length: '长度数据',
+    // precision: '精度数据',
+    // readOnly: '只读数据',
+    // otherConfig: '其他配置数据',
+    // action: '操作数据',
+  })
+}
+
+const handleSubmit = ({ index, key, value }) => {
+
+  data.value[index][key] = value
+  // data.value = data.value.map((item, idx) => {
+  //   index === idx && (item[key] = value)
+  //   return item
+  // })
+
+  console.log(data.value[index])
 }
 </script>
 
