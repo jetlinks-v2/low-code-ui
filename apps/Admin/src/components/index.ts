92d0ef27f7c21490d5cc6e6c8de36c74ca1b3a14
XieYongHong <18010623010@163.com>
2023-08-10 19:58:20 +0800

    feat: 新增DragBox组件

--- apps/Admin/src/components/index.ts
+++ apps/Admin/src/components/index.ts
@@ -3,12 +3,13 @@ import TitleComponent from './TitleComponent/index.vue'
 import ImageUpload from "./Upload/Image/ImageUpload.vue";
 import QuickEditTable from './QuickEditTable/index.vue'
 import Search from './Search'
-
+import DragBox from "./DragBox";
 export default  {
     install(app: App) {
         app.component('TitleComponent', TitleComponent)
           .component('ImageUpload', ImageUpload)
           .component('QuickEditTable', QuickEditTable)
           .component('ProSearch', Search)
+          .component('DragBox', DragBox)
     }
 }
