acf5eb2b00a616de099a01ac66a44662afd2e744
wzyyy <1175160654@qq.com>
2023-08-17 17:50:11 +0800

    feat: content判断

--- apps/Admin/src/views/engine/components/Content/content.vue
+++ apps/Admin/src/views/engine/components/Content/content.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content-warp">
-    <Project :data="props.data.children"/>
-    
+    <Project :data="props.data.children" v-if="props.data.type==='module'"/>
+    <FormDesigner v-else-if="props.data.type === 'page-form'"/>
   </div>
 </template>
 
