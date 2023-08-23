683c6b9b8176863bc13a76d0639ec9f98e21c6af
flykilla <1092112090@qq.com>
2023-08-23 14:08:15 +0800

    update: 更新列表store

--- apps/Admin/src/store/filterModule.ts
+++ apps/Admin/src/store/filterModule.ts
@@ -7,25 +7,44 @@ export const useFilterModuleStore = defineStore('filterModule', () => {
     number: {},
     date: {},
   })
-
-  /**
-   * 设置信息
-   * @param data
-   */
+  const datasource = ref<any>([])
+/**
+ * 设置信息
+ * @param data 
+ * @param type 
+ */
   const setConfigurationInfo = (data: any, type: string) => {
     configurationInfo.value[type] = { ...data }
   }
 
-  /**
-   * 获取信息
-   */
+/**
+ * 获取信息
+ * @param type 
+ * @returns 
+ */
   const getConfigurationInfo = (type: string) => {
     return configurationInfo.value[type]
+  }
+/**
+ * 设置数据
+ * @param data 
+ */
+  const setData = (data: any) => {
+    datasource.value = data
+  }
+/**
+ * 获取数据
+ * @returns 
+ */
+  const getData = () => {
+    return datasource.value
   }
 
   return {
     configurationInfo,
     setConfigurationInfo,
     getConfigurationInfo,
+    setData,
+    getData
   }
 })
