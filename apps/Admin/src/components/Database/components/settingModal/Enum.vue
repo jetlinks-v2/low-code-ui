d261c5f61292696ea46c9d268c99f2f7be08a9cb
XieYongHong <18010623010@163.com>
2023-08-29 11:41:47 +0800

    feat: 完善增删查改
    * fate: 添加增删改查入口
    
    * fix: 修复拖拽异常bug
    
    * fix: 修改左侧树右键菜单功能
    
    * feat: crud新增配置弹窗
    
    * feat: 修改设置弹窗
    
    * feat: 完善增删查改

---
+++ apps/Admin/src/components/Database/components/settingModal/Enum.vue
@@ -0,0 +1,69 @@
+<template>
+  <j-form-item label="数据字典ID" :name="['dictionary', 'dictionaryId']" required>
+    <j-select
+      :options="options"
+      placeholder="请选择数据字典ID"
+      v-model:value="model.dictionary.dictionaryId"
+    />
+  </j-form-item>
+  <j-form-item label="约束" :name="['dictionary', 'multiple']">
+    <BooleanSelect
+      falseText="单选"
+      trueText="多选"
+      v-model:value="model.dictionary.multiple"
+      @change="change"
+    />
+  </j-form-item>
+  <j-form-item label="数据操作" name="spec">
+    <Spec v-model:value="model.spec" />
+  </j-form-item>
+  <j-modal
+    :visible="visible"
+    okText="修改为BIGINT类型"
+    cancelText="忽略"
+    @ok="save"
+    @cancel="visible = false"
+  >
+    <div>
+      当前javaType类型为{{model.jdbcType}}
+    </div>
+    <div>
+      建议修改为BIGINT类型
+    </div>
+  </j-modal>
+</template>
+
+<script setup name="CRUDSettingEnum">
+import { SETTING_FORM_MODEL } from "@/components/Database/util";
+import Spec from './Spec.vue'
+import { inject } from "vue";
+import { useRequest } from '@jetlinks/hooks'
+import { dictionaryList } from "@/api/dictionary";
+
+const { data: options } = useRequest(dictionaryList, {
+  onSuccess(res) {
+    if (res) {
+      return res.result.map(item => ({ ...item, label: item.name, value: item.id }))
+    }
+    return []
+  }
+})
+
+const model = inject(SETTING_FORM_MODEL, {})
+const visible = ref(false)
+const change = (key) => {
+  if (key && model.jdbcType !== 'BIGINT') {
+    visible.value = true
+  }
+}
+
+const save = () => {
+  model.value.jdbcType = 'BIGINT'
+  visible.value = false
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ apps/Admin/src/components/Database/components/settingModal/modal.vue
@@ -0,0 +1,142 @@
+<template>
+  <j-modal
+    visible
+    title="配置"
+    @cancel="cancel"
+    @ok="save"
+  >
+    <j-form
+      ref="formRef"
+      :model="formModel"
+      layout="vertical"
+    >
+      <component :is="componentName" />
+    </j-form>
+  </j-modal>
+</template>
+
+<script setup name="TypeModal">
+import EnumItem from './Enum.vue'
+import ListItem from './List.vue'
+import MapItem from './Map.vue'
+import NumberItem from './Number.vue'
+import StringItem from './String.vue'
+import { SETTING_FORM_MODEL, SETTING_FORM_REF } from '../../util'
+
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+const emit = defineEmits(['cancel', 'save'])
+
+const formRef = ref()
+const formModel = ref({})
+
+provide(SETTING_FORM_MODEL, formModel)
+provide(SETTING_FORM_REF, formRef)
+
+const componentName = computed(() => {
+  const { javaType } = props.data
+
+  const spec = {
+    canImport: false,
+    canExport: false
+  }
+  console.log(javaType)
+  switch(javaType) {
+    case 'Enum':
+      formModel.value = {
+        dictionary: {
+          dictionaryId: undefined,
+          multiple: undefined
+        },
+        spec,
+        ...props.data
+      }
+      return EnumItem;
+    case 'String':
+    case 'Byte':
+    case 'Long':
+      formModel.value = {
+        defaultValueSpec: {
+          fixValue: undefined
+        },
+        validator: {
+          provider: undefined,
+          configuration: {
+            message: undefined,
+            group: undefined
+          }
+        },
+        spec,
+        ...props.data
+      }
+      return StringItem;
+    case 'Double':
+    case 'Int':
+    case 'Float':
+    case 'BigDecimal':
+    case 'BigInteger':
+      formModel.value = {
+        defaultValueSpec: {
+          fixValue: undefined
+        },
+        validator: {
+          provider: undefined,
+          configuration: {
+            message: undefined,
+            group: undefined,
+            classType: javaType,
+            regexp: undefined,
+            min: undefined,
+            max: undefined
+          }
+        },
+        spec,
+        ...props.data
+      }
+      return NumberItem;
+    case 'List':
+      formModel.value = {
+        others: {
+          valueJavaType: undefined
+        },
+        spec,
+        ...props.data
+      }
+      return ListItem;
+    case 'Map':
+      formModel.value = {
+        others: {
+          keyJavaType: undefined,
+          valueJavaType: undefined
+        },
+        spec,
+        ...props.data
+      }
+      return MapItem;
+  }
+})
+
+const cancel = () => {
+  emit('cancel')
+}
+const save = () => {
+  formRef.value.validate().then(res => {
+    if (res) {
+      emit('save', {
+        ...props.data,
+        ...formModel.value
+      })
+    }
+  })
+}
+
+</script>
+
+<style scoped>
+
+</style>
