2729c59431a7000db9a3a9d90021048b68cc9f09
leiqiaochu <leiqiaochu@aossci.com>
2023-08-30 09:53:29 +0800

    feat: 表单左侧tab

---
+++ apps/Admin/src/components/FormDesigner/components/Panels/Filed/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <j-tabs v-model:activeKey="activeKey" :tabBarGutter="0" type="card">
+        <j-tab-pane tab="组件库" key="module"><Library/></j-tab-pane>
+        <j-tab-pane tab="模板" key="template"><Library/></j-tab-pane>
+    </j-tabs>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Library from "./library";
+const activeKey = ref('module')
+</script>
+<style lang="less" scoped>
+:deep(.ant-tabs-tab){
+    width: 146px;
+}
+:deep(.ant-tabs-tab-btn){
+        margin: 0 auto;
+}
+</style>
