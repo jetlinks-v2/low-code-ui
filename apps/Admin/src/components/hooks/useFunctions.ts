5721dec25f210d02905264e09f58207dc57d044a
haodd <1903289197@qq.com>
2023-08-31 11:29:21 +0800

    feat: 后端功能-指令公用函数

---
+++ apps/Admin/src/components/hooks/useFunctions.ts
@@ -0,0 +1,61 @@
+import { useProduct } from "@/store";
+import { queryCommand } from "@/api/project";
+import { providerEnum } from "../ProJect";
+
+type CommandType = {
+  id: string
+  name: string
+  description: string
+  inputs: any[]
+  output: Record<string, any>
+}
+export const useFunctions = () => {
+  const functionOptions = ref<Draft.Function[]>([])
+  const commandOptions = ref<CommandType[]>([])
+  const pages = ref<Draft.Resource[]>([])
+  const { data } = useProduct()
+  
+  /**
+   * 获取草稿下的所有功能和页面资源
+   */
+  const findFunctionsPages = (data: any[]) => {
+    data.forEach((item) => {
+      if (item.functions && item.functions.length) {
+        functionOptions.value.push(...item.functions)
+      }
+      if (item.type == providerEnum.Page) {
+        pages.value.push(item)
+      }
+      if (item.children) {
+        findFunctionsPages(item.children)
+      }
+    })
+  }
+  findFunctionsPages(data)
+
+  /***
+   * 查询功能下的的指令
+   * @param functionId 功能id
+   */
+  const handleFunction = async (functionId: string) => {
+    const params = {
+      modules: [
+          {
+          id: data?.[0].id,
+          name: data?.[0].name,
+          functions: functionOptions!.value?.filter(item => item.id === functionId)
+        }
+      ]
+    }
+    const res = await queryCommand(params)
+    if(res.success) {
+      commandOptions.value = res.result?.[0]?.command
+    }
+  }
+  return {
+    functionOptions,
+    commandOptions,
+    pages,
+    handleFunction,
+  }
+}
