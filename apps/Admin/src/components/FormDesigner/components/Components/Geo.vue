72dbc805687be562c00a0f6f3946e1b0b9279184
leiqiaochu <leiqiaochu@aossci.com>
2023-08-31 10:10:19 +0800

    feat: 地图组件删减

--- apps/Admin/src/components/FormDesigner/components/Components/Geo.vue
+++ apps/Admin/src/components/FormDesigner/components/Components/Geo.vue
@@ -1,11 +1,10 @@
 <template>
-    <j-cascader :options="geoList"  v-model:value="_value" @change="changeValue" :disabled="disabled" changeOnSelect :showSearch="{filter}">     
+    <j-cascader :options="geoList"  :value="_value" @change="changeValue" :disabled="disabled" changeOnSelect :showSearch="{filter}">     
     </j-cascader>
 </template>
 
 <script lang="ts" setup>
 import { cloneDeep,debounce } from 'lodash-es';
-import Geo from '../../../../../public/json/geo.json'
 const props = defineProps({
     value:{
         type:String,
@@ -19,37 +18,15 @@ const props = defineProps({
 const emit = defineEmits(['update:value'])
 const geoList = ref()
 const _value  = ref()
-//处理前端geo数据
-const getList = (list:any,parentName?:string) =>{
-   return list.map((item:any)=>{
-        const optionValue = parentName ? parentName + '-' + item.name : item.name
-        if(item?.districts?.length){
-           const children = getList(item.districts,optionValue)
-           return {
-            label:item.name,
-            value:optionValue,
-            children:children
-           }
-        }else{
-            return {
-                label:item.name,
-                value:optionValue
-            }
-        }
-    })
+const changeValue = (value) =>{
+    emit('update:value',value)
 }
-const filter = (inputValue, path) => {
-  return path.some(option => option.value.includes(inputValue));
-}
-const changeValue = () =>{
-    emit('update:value',_value.value)
-}
-onMounted(()=>{ 
-    const list = cloneDeep(Geo)
-    geoList.value = getList(list)
-})
+
 watch(()=>props.value,()=>{
     _value.value = props.value
+},{
+    deep:true,
+    immediate:true
 })
 </script>
 <style lang="less" scoped>
