ed69749b542180064c1b4e06347d748d58090c2c
wzyyy <1175160654@qq.com>
2023-09-08 17:58:36 +0800

    fix: 菜单传参及底部路径

--- apps/Admin/src/views/engine/index.vue
+++ apps/Admin/src/views/engine/index.vue
@@ -3,8 +3,11 @@
     <Header />
     <DragContent>
       <div class="engine-warp">
-          <Tree />
-          <Content />
+        <Tree />
+        <Content />
+      </div>
+      <div class="path">
+        <div style="margin-left: 10px;">{{ path }}</div>
       </div>
     </DragContent>
   </div>
@@ -12,6 +15,20 @@
 
 <script setup name="Engine">
 import { Header, Tree, Content } from './components'
+import { useProduct, useEngine } from '@/store'
+const engine = useEngine()
+const product = useProduct()
+
+const path = ref()
+
+watchEffect(() => {
+  if (engine.activeFile) {
+    const data = product.getById(engine.activeFile)
+    path.value = product.getParent(data).map((item) => item.title).join(' -> ')
+    console.log('data-------', path.value)
+  }
+
+})
 
 </script>
 
@@ -25,6 +42,12 @@ import { Header, Tree, Content } from './components'
   .engine-warp {
     display: flex;
     height: calc(100% - 56px);
+  }
+  .path{
+    background-color: rgb(216, 216, 216);
+    z-index: 999;
+    height: 30px;
+    line-height: 30px;
   }
 }
 </style>
