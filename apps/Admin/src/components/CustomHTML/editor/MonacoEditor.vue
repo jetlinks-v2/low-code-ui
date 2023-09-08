926bea6995dda691be0ae9a8ac6d2ad11444d51e
YouLin <92132544+holt230@users.noreply.github.com>
2023-09-08 16:58:36 +0800

    update: 更新html代码编辑器失焦保存
    * update: 更新html代码编辑器失焦保存

--- apps/Admin/src/components/CustomHTML/editor/MonacoEditor.vue
+++ apps/Admin/src/components/CustomHTML/editor/MonacoEditor.vue
@@ -3,7 +3,7 @@
   value: string,
   filename: string
 }>()
-const emit = defineEmits<{ (e: 'change', code: string): void }>()
+const emit = defineEmits<{ (e: 'change', code: string): void, (e: 'blur'): void }>()
 
 defineOptions({
   editorType: 'monaco',
@@ -13,8 +13,12 @@ const code = ref(props.value)
 const onChange = (code: string) => {
   emit('change', code)
 }
+
+const onBlur = () => {
+  emit('blur')
+}
 </script>
 
 <template>
-  <j-monaco-editor v-model="code"  @change="onChange" language="html"/>
+  <j-monaco-editor v-model="code"  @change="onChange" @blur="onBlur" language="html"/>
 </template>
