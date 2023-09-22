951753daf5c6f9485eb721c1269e043532f3577b
XieYongHong <18010623010@163.com>
2023-09-22 14:36:45 +0800

    fix: bug#18615
    * fix: bug#18612
    
    * fix: bug#18615

--- apps/Admin/src/components/QuickEditTable/util.ts
+++ apps/Admin/src/components/QuickEditTable/util.ts
@@ -1,5 +1,6 @@
 import { randomString } from '@jetlinks/utils'
 import Schema from 'async-validator';
+import {isArray} from "lodash-es";
 
 export const dataAddID = (data: any[], rowHeight: number) => {
   return data.map((item, index ) => {
@@ -13,9 +14,39 @@ export const dataAddID = (data: any[], rowHeight: number) => {
   })
 }
 
+export const proAll = (array: Array<Promise<any>>) => {
+  return new Promise((resolve, reject) => {
+    const length = array.length
+    const error: any = {}
+    const success: any[] = []
+    let count = 0
+
+    const jump = () => {
+      if (count >= length) {
+        error.length ? reject(error) : resolve(success)
+      }
+    }
+
+    for (let i=0;i<length;i++) {
+      array[i].then(r => {
+        success.push(r)
+        count++
+        jump()
+      }, (e) => {
+        console.log('proAll for', e)
+        Object.assign(error, e)
+        console.log('proAll', e)
+        count++
+        jump()
+      })
+    }
+  })
+}
+
 export const useValidate = (dataSource) => {
   const validateRef = ref()
   const errorMap = ref({})
+  const watch = {}
 
   let ruleObj = {}
 
@@ -26,8 +57,17 @@ export const useValidate = (dataSource) => {
   const handleColumns = (columns) => {
     ruleObj = {}
     columns.forEach(item => {
-      if (item.form && item.form.rules) {
-        ruleObj[item.dataIndex] = item.form.rules
+      if (item.form) {
+        if (item.form.rules) {
+          ruleObj[item.dataIndex] = item.form.rules
+        }
+
+        if (item.form.watch && isArray(item.form.watch)) {
+          item.form.watch.forEach(key => {
+            ruleObj[key] = item.form.rules
+            watch[key] = item.dataIndex
+          })
+        }
       }
     })
   }
@@ -48,16 +88,22 @@ export const useValidate = (dataSource) => {
           { [name]: value, record },
           { firstFields: true },
           (err) => {
-            const hasName = err?.find(item => item.field === name)
 
+            const hasName = err?.find(item => item.field === name)
             const path = createPath(name, record._quick_id)
             if (err && hasName) { // 有错误
-              errorMap.value[path] = hasName.message
-              reject({ [path]:[hasName] })
+              if (!watch[hasName.field]) {
+                errorMap.value[path] = hasName.message
+                reject({ [path]:[hasName] })
+              }
             } else { // 无错误
               resolve(false)
               if (errorMap.value[path]) {
                 delete errorMap.value[path]
+              }
+              if (watch[name]) {
+                const watchPath = createPath(watch[name], record._quick_id)
+                delete errorMap.value[watchPath]
               }
             }
           }
@@ -69,16 +115,23 @@ export const useValidate = (dataSource) => {
   const validates = () => {
     return new Promise(async (resolve, reject) => {
       let errorMsg = {}
+
       for (const item of dataSource) {
         for (const key in ruleObj) {
-          await validate(key, item[key], item)?.catch(e => {
-            errorMsg = {
-              ...e,
-              ...errorMsg
-            }
-          })
+          // if (!watch[key]) {
+          //   validatePromise.push(validate(key, item[key], item))
+          // }
+          if (!watch[key]) {
+            await validate(key, item[key], item)?.catch(e => {
+              errorMsg = {
+                ...e,
+                ...errorMsg
+              }
+            })
+          }
         }
       }
+
       if (Object.keys(errorMsg).length) {
         reject(errorMsg)
       } else {
--- apps/Admin/src/views/preview/index.vue
+++ apps/Admin/src/views/preview/index.vue
@@ -1,28 +1,44 @@
 <template>
-  <ListPage v-if="showList" :data="data" :show="true" />
-  <HtmlPage v-else-if="showHtml" :code="data" />
+  <template v-if="!isEmpty">
+    <ListPage v-if="showList" :data="data" :show="true" :projectId="route.params.project" />
+    <HtmlPage v-else-if="showHtml" :code="data" />
+  </template>
+  <template v-else>
+    <Result status="404" title="404"></Result>
+  </template>
 </template>
 
 <script setup name="Preview">
 import { getResource } from '@/api/basis'
 import ListPage from '@/components/ListPage/Output/index.vue'
 import HtmlPage from '@/components/CustomHTML/output/Preview.vue'
+import { queryProject } from "@/api/project";
+import { Result } from 'jetlinks-ui-components'
 
 const route = useRoute()
 const data = ref()
+const isEmpty = ref(false)
+
 
 const showList = computed(() => {
-  return route.params.type === 'list' && data.value
+  return route.params?.type === 'list' && data.value
 })
 
 const showHtml = computed(() => {
-  return route.params.type === 'html' && data.value
+  return route.params?.type === 'html' && data.value
 })
 
 const getInfo = async () => {
   const { project, module, id } = route.params
-  getResource(project, module, id).then(resp => {
-    data.value = resp
+  queryProject({ terms: [{ value: project, termType: 'eq', column: 'id'}]}).then(resp => {
+    if (resp.result && resp.result.data?.length && resp.result.data[0].runningState?.value === 'enabled') {
+      isEmpty.value = false
+      getResource(project, module, id).then(resp => {
+        data.value = typeof resp === 'string' ? resp : JSON.stringify(resp)
+      })
+    } else {
+      isEmpty.value = true
+    }
   })
 }
 
