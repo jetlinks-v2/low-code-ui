060ee137ff116144918868aaa23d9b25e5a00b49
XieYongHong <18010623010@163.com>
2023-09-27 14:54:59 +0800

    fix: bug#18491

--- apps/Admin/src/components/Database/table.vue
+++ apps/Admin/src/components/Database/table.vue
@@ -27,9 +27,9 @@
           <span v-if="index <= maxLen">{{ record.name }}</span>
           <j-input v-else v-model:value="record.name" @change="() => { valueChange(record.name); alias(record.name, record) }" />
         </template>
-        <template #comment="{record, index}">
+        <template #comment="{record, index, valueChange}">
           <span v-if="index <= maxLen">{{ record.comment }}</span>
-          <j-input v-else v-model:value="record.comment" :maxLength="16" @change="emitUpdateDataSource"/>
+          <j-input v-else v-model:value="record.comment" :maxLength="16" @change="() => { valueChange(record.comment); emitUpdateDataSource()}"/>
         </template>
         <template #javaType="{record, index, valueChange}">
           <span v-if="index <= maxLen">{{record.javaType}}</span>
