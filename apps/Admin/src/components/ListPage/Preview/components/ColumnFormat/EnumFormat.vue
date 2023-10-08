6058b8b54d3f09e9ebdb7fdf73b382c7e252c703
haodd <1903289197@qq.com>
2023-10-08 15:03:56 +0800

    fix: 枚举类型数据格式化

--- apps/Admin/src/components/ListPage/Preview/components/ColumnFormat/EnumFormat.vue
+++ apps/Admin/src/components/ListPage/Preview/components/ColumnFormat/EnumFormat.vue
@@ -1,9 +1,11 @@
 <template>
-  <span>{{ formatValue }}</span>
+  <ArrayFormat :config="config" :value="formatValue" v-if="isArray"/>
+  <span v-else>{{ formatValue }}</span>
 </template>
 
 <script setup lang="ts">
 import { PropType } from 'vue';
+import ArrayFormat from './ArrayFormat.vue';
 
 
 const props = defineProps({
@@ -17,11 +19,18 @@ const props = defineProps({
   }
 })
 
+const isArray = computed(() => {
+  return Array.isArray(props.value)
+})
 /**
- * int, long, text, double, float, string格式化
+ * 枚举类型格式化
  */
 
 const formatValue = computed(() => {
-  return props.value?.text || '--'
+  if(isArray.value) {
+    return props.value?.map(item => item.text)
+  } else {
+    return props.value?.text || '--'
+  }
 })
 </script>
