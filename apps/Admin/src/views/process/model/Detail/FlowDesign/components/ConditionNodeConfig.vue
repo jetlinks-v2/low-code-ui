30911dad8cd3a8c7638cd7124f891c6eb4dae90f
JiangQiming <291854119@qq.com>
2023-10-11 15:55:30 +0800

    feat: 条件节点, 条件配置组件开发

--- apps/Admin/src/views/process/model/Detail/FlowDesign/components/ConditionNodeConfig.vue
+++ apps/Admin/src/views/process/model/Detail/FlowDesign/components/ConditionNodeConfig.vue
@@ -4,12 +4,8 @@
     <j-tab-pane key="basic" tab="基础配置">
       <j-form ref="basicFormRef" :model="basicFormData" layout="vertical">
         <h3>条件配置</h3>
-        <j-form-item label="请配置进入下方节点的条件" name="forms">
-          <Term
-            ref="termRef"
-            :variableData="propertyOption"
-            :formData="formData"
-          ></Term>
+        <j-form-item label="请配置进入下方节点的条件" name="conditions">
+          <ConditionSelect v-model:value="basicFormData.conditions" />
         </j-form-item>
       </j-form>
     </j-tab-pane>
@@ -17,9 +13,14 @@
 </template>
 
 <script setup lang="ts">
+import { findNodeById } from './utils'
+import { useFlowStore } from '@/store/flow'
+import ConditionSelect from './ConditionSelect.vue'
+
+const flowStore = useFlowStore()
 const activeKey = ref('basic')
 const props = defineProps({
-  config: {
+  node: {
     type: Object,
     default: () => ({}),
   },
@@ -28,246 +29,32 @@ const props = defineProps({
 // 基础配置
 const basicFormRef = ref()
 const basicFormData = ref({
-  forms: [],
+  conditions: props.node?.props?.conditions,
 })
 
-const propertyOption = [
-  {
-    column: 'timestamp',
-    name: '上报时间',
-    fullName: '上报时间',
-    description: '数据上报时指定的时间',
-    dataType: 'date',
-    metadata: false,
-    termTypes: [
-      {
-        id: 'eq',
-        name: '等于',
-      },
-      {
-        id: 'neq',
-        name: '不等于',
-      },
-      {
-        id: 'gt',
-        name: '大于',
-      },
-      {
-        id: 'gte',
-        name: '大于等于',
-      },
-      {
-        id: 'lt',
-        name: '小于',
-      },
-      {
-        id: 'lte',
-        name: '小于等于',
-      },
-      {
-        id: 'btw',
-        name: '在...之间',
-      },
-      {
-        id: 'nbtw',
-        name: '不在...之间',
-      },
-      {
-        id: 'time_gt_now',
-        name: '距离当前时间大于...秒',
-      },
-      {
-        id: 'time_lt_now',
-        name: '距离当前时间小于...秒',
-      },
-    ],
-  },
-  {
-    column: 'total_power_factor',
-    name: '总功率因素',
-    fullName: '总功率因素',
-    description: '属性',
-    dataType: 'boolean',
-    metadata: true,
-    termTypes: [
-      {
-        id: 'eq',
-        name: '等于',
-      },
-      {
-        id: 'neq',
-        name: '不等于',
-      },
-      {
-        id: 'gt',
-        name: '大于',
-      },
-      {
-        id: 'gte',
-        name: '大于等于',
-      },
-      {
-        id: 'lt',
-        name: '小于',
-      },
-      {
-        id: 'lte',
-        name: '小于等于',
-      },
-      {
-        id: 'btw',
-        name: '在...之间',
-      },
-      {
-        id: 'nbtw',
-        name: '不在...之间',
-      },
-      {
-        id: 'in',
-        name: '在...之中',
-      },
-      {
-        id: 'nin',
-        name: '不在...之中',
-      },
-    ],
-    metrics: [],
-    children: [
-      {
-        column: 'properties.total_power_factor.last',
-        name: '上一值',
-        fullName: '总功率因素/上一值',
-        description: '上一次上报的数据值',
-        dataType: 'double',
-        metadata: true,
-        termTypes: [
-          {
-            id: 'eq',
-            name: '等于',
-          },
-          {
-            id: 'neq',
-            name: '不等于',
-          },
-          {
-            id: 'gt',
-            name: '大于',
-          },
-          {
-            id: 'gte',
-            name: '大于等于',
-          },
-          {
-            id: 'lt',
-            name: '小于',
-          },
-          {
-            id: 'lte',
-            name: '小于等于',
-          },
-          {
-            id: 'btw',
-            name: '在...之间',
-          },
-          {
-            id: 'nbtw',
-            name: '不在...之间',
-          },
-          {
-            id: 'in',
-            name: '在...之中',
-          },
-          {
-            id: 'nin',
-            name: '不在...之中',
-          },
-        ],
-        metrics: [],
-      },
-    ],
-  },
-  {
-    column: 'degrees',
-    name: '表计度数',
-    fullName: '表计度数',
-    description: '属性',
-    dataType: 'double',
-    metadata: true,
-    termTypes: [
-      {
-        id: 'eq',
-        name: '等于',
-      },
-      {
-        id: 'neq',
-        name: '不等于',
-      },
-      {
-        id: 'gt',
-        name: '大于',
-      },
-      {
-        id: 'gte',
-        name: '大于等于',
-      },
-      {
-        id: 'lt',
-        name: '小于',
-      },
-      {
-        id: 'lte',
-        name: '小于等于',
-      },
-      {
-        id: 'btw',
-        name: '在...之间',
-      },
-      {
-        id: 'nbtw',
-        name: '不在...之间',
-      },
-      {
-        id: 'in',
-        name: '在...之中',
-      },
-      {
-        id: 'nin',
-        name: '不在...之中',
-      },
-    ],
-    metrics: [],
-    children: [
-      {
-        column: 'properties.degrees.last',
-        name: '上一值',
-        fullName: '表计度数/上一值',
-        description: '上一次上报的数据值',
-        dataType: 'double',
-        metadata: true,
-        termTypes: [
-          {
-            id: 'eq',
-            name: '等于',
-          },
-        ],
-        metrics: [],
-      },
-    ],
-  },
-]
-
-const formData = [
-  {
-    column: 'properties.total_power_factor.last',
-    operator: 'eq',
-    value: 12,
-  },
-  {
-    column: 'properties.degrees.last',
-    operator: 'eq',
-    value: '123',
-  },
-]
+/**
+ * 将数据保存至pinia
+ */
+const saveConfigToPinia = () => {
+  return new Promise((resolve, reject) => {
+    basicFormRef.value
+      .validate()
+      .then((valid) => {
+        const result = findNodeById(
+          flowStore.model.nodes,
+          flowStore.selectedNode.id,
+        )
+        result.props = { ...result.props, ...basicFormData }
+        resolve(valid)
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
+defineExpose({
+  saveConfigToPinia,
+})
 </script>
 
 <style lang="less" scoped></style>
---
+++ apps/Admin/src/views/process/model/Detail/FlowDesign/components/ConditionSelect.vue
@@ -0,0 +1,202 @@
+<template>
+  <div
+    class="condition-select"
+    v-for="(item, index) in conditionSelect"
+    :key="index"
+  >
+    <a-tree-select
+      v-model:value="item.condition"
+      v-model:searchValue="item.searchValue"
+      show-search
+      placeholder="请选择"
+      allow-clear
+      tree-default-expand-all
+      tree-node-filter-prop="label"
+      :tree-data="conditionOptions"
+      :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+    >
+      <template #title="{ value: val, label }">
+        <template
+          v-for="(fragment, i) in label
+            ?.toString()
+            ?.split(
+              new RegExp(
+                `(?<=${item.searchValue})|(?=${item.searchValue})`,
+                'i',
+              ),
+            )"
+        >
+          <span
+            v-if="fragment.toLowerCase() === item.searchValue.toLowerCase()"
+            :key="i"
+            style="color: #08c"
+          >
+            {{ fragment }}
+          </span>
+          <template v-else>{{ fragment }}</template>
+        </template>
+      </template>
+    </a-tree-select>
+    <j-select
+      v-model:value="item.termsType"
+      :options="termsOptions"
+      placeholder="请选择"
+    />
+
+    <j-select
+      v-model:value="item.result"
+      :options="valueOptions"
+      placeholder="请选择"
+      :mode="['eq', 'neq'].includes(item.termsType) ? 'combobox' : 'multiple'"
+    />
+    <AIcon
+      type="DeleteOutlined"
+      style="color: red"
+      @click="handleRemove(index)"
+      v-if="index !== 0"
+    />
+  </div>
+  <j-button type="primary" block size="small" ghost @click="handleAdd">
+    +
+  </j-button>
+</template>
+
+<script setup lang="ts">
+interface IConditionSelect {
+  condition: string
+  termsType: string
+  result: string | string[]
+  searchValue: string
+}
+
+const emit = defineEmits(['update:value'])
+const props = defineProps({
+  value: {
+    type: Array as PropType<IConditionSelect[]>,
+    default: () => [
+      {
+        condition: undefined,
+        termsType: undefined,
+        result: undefined,
+        searchValue: '',
+      },
+    ],
+  },
+})
+
+const conditionSelect = ref<IConditionSelect[]>([])
+watch(
+  () => props.value,
+  (val) => {
+    conditionSelect.value = val
+  },
+  { deep: true, immediate: true },
+)
+// const conditionSelect = computed({
+//   get: () => props.value,
+//   set: (val) => {
+//     console.log('conditionSelect.value1: ', val)
+//     emit('update:value', val)
+//   },
+// })
+
+const conditionOptions = ref([
+  {
+    label: '审批节点',
+    value: 'node-0',
+    children: [
+      {
+        label: '审批人',
+        value: 'node-0-1',
+      },
+    ],
+  },
+  {
+    label: '办理节点',
+    value: 'node-1',
+    children: [
+      {
+        label: '办理人',
+        value: 'node-1-1',
+      },
+    ],
+  },
+  {
+    label: '请假申请',
+    value: 'node-2',
+    children: [
+      {
+        label: '请假人',
+        value: 'node-2-1',
+      },
+      {
+        label: '请假时间',
+        value: 'node-2-2',
+      },
+    ],
+  },
+])
+
+const termsOptions = ref([
+  {
+    label: '=',
+    value: 'eq',
+  },
+  {
+    label: '!=',
+    value: 'neq',
+  },
+  {
+    label: '包含',
+    value: 'include',
+  },
+  {
+    label: '不包含',
+    value: 'ninclude',
+  },
+])
+const valueOptions = ref([
+  {
+    label: '表单提交人',
+    value: 'id1',
+  },
+  {
+    label: '节点审核人',
+    value: 'id2',
+  },
+  {
+    label: '节点办理人',
+    value: 'id3',
+  },
+])
+
+const handleRemove = (index: number) => {
+  conditionSelect.value.splice(index, 1)
+}
+
+const handleAdd = () => {
+  conditionSelect.value.push({
+    condition: undefined,
+    termsType: undefined,
+    result: undefined,
+    searchValue: '',
+  })
+}
+
+watch(
+  () => conditionSelect.value,
+  (val) => {
+    emit('update:value', val)
+  },
+  { deep: true },
+)
+</script>
+
+<style lang="less" scoped>
+.condition-select {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 10px;
+}
+</style>
