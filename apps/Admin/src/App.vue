50ae48289532bcd398917b3d3f2a071d6c81959e
XieYongHong <18010623010@163.com>
2023-10-11 18:15:23 +0800

    fix: buf#19019、19020

--- apps/Admin/src/App.vue
+++ apps/Admin/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <ConfigProvider :locale="zhCN">
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <keep-alive>
+        <component :is="Component" :key="$route.name" v-if="$route.meta.keepAlive" />
+      </keep-alive>
+      <component :is="Component" :key="$route.name" v-if="!$route.meta.keepAlive" />
+    </router-view>
   </ConfigProvider>
 </template>
 <script setup lang="ts">
