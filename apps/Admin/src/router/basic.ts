d82b89301ac99c858ec7345aad1df069e89bc236
hear <70554662+Wzyyy98@users.noreply.github.com>
2023-10-19 18:11:15 +0800

    feat: 待办流程暂存数据
    * feat: 可编辑表格
    
    * feat: 待办可编辑表格
    
    * fix: 流程记录
    
    * feat: 待办流程暂存数据

--- apps/Admin/src/router/basic.ts
+++ apps/Admin/src/router/basic.ts
@@ -68,6 +68,27 @@ export const BASIC_ROUTER_DATA: any[] = [
         name: '流程表单',
         url: '/flow-engine/form'
       },
+      {
+        
+        code: 'process/me/todo',
+        name: '待办事项',
+        url: '/flow-engine/me/todo'
+      },
+      {
+        code: 'process/me',
+        name: '已办事项',
+        url: '/flow-engine/me/finished'
+      },
+      {
+        code: 'process/me/cc',
+        name: '抄送给我',
+        url: '/flow-engine/me/cc'
+      },
+      {
+        code: 'process/me/initiate',
+        name: '我发起的',
+        url: '/flow-engine/me/initiate'
+      }
     ]
   },
 ]
@@ -182,6 +203,118 @@ export const BASIC_ROUTERS: RouteRecordItem[] = [
           title: '流程模型'
         },
         children: [
+            {
+                path: '/flow-engine/model',
+                name: 'flow-engine/model',
+                component: () => import('@/views/process/model/index.vue'),
+                meta: {
+                    title: '流程模型'
+                },
+                children: [
+                    {
+                        path: 'detail',
+                        name: 'detail',
+                        component: () => import('@/views/process/model/Detail/index.vue'),
+                        meta: {
+                            title: '流程详情'
+                        }
+                    },
+                ]
+            },
+            {
+                path: '/flow-engine/instance',
+                name: 'Instance',
+                component: () => import('@/views/process/instance/index.vue'),
+                meta: {
+                    title: '流程实例',
+                },
+            },
+            {
+                path: '/flow-engine/monitor',
+                name: 'Monitor',
+                component: () => import('@/views/process/monitor/index.vue'),
+                meta: {
+                    title: '流程监控',
+                },
+            },
+            {
+                path: '/flow-engine/initiate',
+                name: 'Initiate',
+                component: () => import('@/views/process/initiate/index.vue'),
+                meta: {
+                    title: '发起流程',
+                },
+                children: [
+                    {
+                        path: 'initiate-detail',
+                        name: 'initiate-detail',
+                        component: () => import('@/views/process/initiate/Detail/index.vue'),
+                        meta: {
+                            title: '流程详情'
+                        }
+                    },
+                ]
+            },
+            {
+                path: '/flow-engine/me',
+                name: 'Me',
+                // component: () => import('@/views/process/me/index.vue'),
+                meta: {
+                    title: '我的流程',
+                },
+                children: [
+                    {
+                        path: '/flow-engine/me/todo',
+                        name: 'todo',
+                        component: () => import('@/views/process/me/index.vue'),
+                        meta: {
+                            title: '待办事项'
+                        }
+                    },
+                    {
+                        path: '/flow-engine/me/finished',
+                        name: 'finished',
+                        component: () => import('@/views/process/me/index.vue'),
+                        meta: {
+                            title: '已办事项'
+                        }
+                    },
+                    {
+                        path: '/flow-engine/me/cc',
+                        name: 'cc',
+                        component: () => import('@/views/process/me/index.vue'),
+                        meta: {
+                            title: '抄送给我'
+                        }
+                    },
+                    {
+                        path: '/flow-engine/me/initiate',
+                        name: 'initiate',
+                        component: () => import('@/views/process/me/index.vue'),
+                        meta: {
+                            title: '我发起的'
+                        }
+                    },
+                ]
+            },
+            {
+                path: '/flow-engine/form',
+                name: 'Form',
+                component: () => import('@/views/process/form/index.vue'),
+                meta: {
+                    title: '流程表单',
+                },
+                children: [
+                    {
+                        path: 'detail/:id',
+                        name: 'detail/:id',
+                        component: () => import('@/views/process/form/Detail/index.vue'),
+                        meta: {
+                            title: '我的表单'
+                        }
+                    },
+                ]
+            },
           {
             path: 'detail',
             name: 'detail',
