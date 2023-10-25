8981b7a8b495500cd9b0fd53fed5d5f06c23868b
liangmou <1414733123@qq.com>
2023-10-25 11:09:37 +0800

    feat: 流程引擎首页

---
+++ apps/Admin/src/views/process/components/MyCard/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="my-card" @click="handleClick">
+    <j-badge :count="getCount()">
+      <div class="center">
+        <div class="icon">
+          <ProImage :src="getImage(`${data.icon}`)" :preview="false" />
+        </div>
+        <div class="name">{{ data.name }}</div>
+      </div>
+    </j-badge>
+  </div>
+</template>
+<script setup lang="ts">
+import { getImage } from '@jetlinks/utils'
+
+const props = defineProps({
+  data: {
+    type: Object as PropType<any>,
+    default: () => ({}),
+  },
+})
+const router = useRouter()
+
+const handleClick = () => {
+  router.push({
+    path: props.data.path,
+  })
+}
+
+const getCount = () => {
+  return 0
+}
+</script>
+<style scoped lang="less">
+.my-card {
+  width: 320px;
+  height: 240px;
+  padding: 30px 80px;
+  border-radius: 6px;
+  border: 2px solid #ffffff;
+  background: linear-gradient(180deg, #ffffff -3%, #ffffff 18%, #e6eefc 99%);
+
+  .center {
+    width: 160px;
+    height: 180px;
+
+    .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 160px;
+      height: 140px;
+    }
+    .name {
+      text-align: center;
+      font-size: 16px;
+      font-weight: normal;
+      line-height: 20px;
+      color: #000000;
+    }
+  }
+}
+</style>
---
+++ apps/Admin/src/views/process/home/index.vue
@@ -0,0 +1,62 @@
+<!-- 流程引擎 -->
+<template>
+  <page-container>
+    <TitleComponent data="流程搭建" />
+    <div class="process">
+      <MyCard v-for="item of processBuild" :data="item"></MyCard>
+    </div>
+
+    <TitleComponent data="我的流程" />
+    <div class="process">
+      <MyCard v-for="item of myProcess" :data="item"></MyCard>
+    </div>
+  </page-container>
+</template>
+<script setup>
+import MyCard from '../components/MyCard/index.vue'
+import { getMeProcessList } from '@/api/process/me'
+
+const processBuild = [
+  { name: '流程模型', icon: '/process/model.png', path: '/flow-engine/model' },
+  { name: '流程表单', icon: '/process/form.png', path: '/flow-engine/form' },
+  {
+    name: '流程监控',
+    icon: '/process/monitor.png',
+    path: '/flow-engine/monitor',
+  },
+  {
+    name: '流程实例',
+    icon: '/process/instance.png',
+    path: '/flow-engine/instance',
+  },
+]
+
+const myProcess = [
+  {
+    name: '发起流程',
+    icon: '/process/initiate.png',
+    path: '/flow-engine/initiate',
+  },
+  { name: '待办事项', icon: '/process/todo.png', path: '/flow-engine/me/todo' },
+  {
+    name: '已办事项',
+    icon: '/process/finished.png',
+    path: '/flow-engine/me/finished',
+  },
+  {
+    name: '我发起的',
+    icon: '/process/me-initiate.png',
+    path: '/flow-engine/me/initiate',
+  },
+  { name: '抄送给我', icon: '/process/cc.png', path: '/flow-engine/me/cc' },
+]
+</script>
+<style scoped lang="less">
+.process {
+  display: flex;
+  justify-content: start;
+  flex-wrap: wrap;
+  gap: 40px;
+  margin-bottom: 80px;
+}
+</style>
