f91fef165f6b7b1c770b486073d6d3fbab2cfe53
liangMou-q <71762454+liangMou-q@users.noreply.github.com>
2023-10-26 09:55:27 +0800

    fix: 表格表单空值判断
    * fix: 修改bug
    
    * fix: 表格表单空值判断

--- apps/Admin/src/api/process/initiate.ts
+++ apps/Admin/src/api/process/initiate.ts
@@ -11,8 +11,8 @@ export const getList_api = (data: any) =>
  * 发起流程
  * @returns
  */
-export const start_api = ({ data, start }: any) =>
-  request.postParams(`/process/runtime/${data.id}/_create`, data, { start })
+export const start_api = ({ id, data, start }: any) =>
+  request.postParams(`/process/runtime/${id}/_create`, data, { start })
 
 /**
  * 获取流程详情
@@ -20,3 +20,11 @@ export const start_api = ({ data, start }: any) =>
  */
 export const processDetail_api = (id: string) =>
   request.get(`/process/runtime/process/detail/${id}`)
+
+  /**
+ * 有草稿时保存
+ * @returns
+ */
+export const save_api = ({ id, data, start }: any) =>
+request.postParams(`/process/runtime/${id}/_save`, data, { start })
+
