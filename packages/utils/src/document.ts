56e29e077f66c2c443518b4b30f70a726b3bbcd5
孙超 <54933585+sun-chaochao@users.noreply.github.com>
2023-11-14 18:03:09 +0800

    fix: downloadFileByUrl
    * fix: downloadFileByUrl

--- packages/utils/src/document.ts
+++ packages/utils/src/document.ts
@@ -7,10 +7,10 @@ export const buildScriptTag = (src: string): HTMLScriptElement => {
   return script;
 };
 
-export const downloadFileByUrl = (url: string, name: string, type: string) => {
+export const downloadFileByUrl = (url: string, name: string, type?: string) => {
   const downNode = document.createElement('a');
   downNode.style.display = 'none';
-  downNode.download = `${name}.${type}`;
+  downNode.download = type ? `${name}.${type}` : name;
   downNode.href = url;
   document.body.appendChild(downNode);
   downNode.click();
