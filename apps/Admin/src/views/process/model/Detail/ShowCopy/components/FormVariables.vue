444d5065624c2d4eb7a1171c5341fa0820a4103e
孙超 <54933585+sun-chaochao@users.noreply.github.com>
2023-11-17 10:14:12 +0800

    fix: 20415
    * fix: 20415

--- apps/Admin/src/views/process/model/Detail/ShowCopy/components/FormVariables.vue
+++ apps/Admin/src/views/process/model/Detail/ShowCopy/components/FormVariables.vue
@@ -5,6 +5,8 @@
     width="1000px"
     @ok="handleOk"
     @cancel="_visible = false"
+    :maskClosable="false"
+    :destroyOnClose="true"
   >
     <template #title>
       <j-space>
@@ -68,6 +70,7 @@ import { queryFormNoPage_api } from '@/api/process/model'
 import { useFlowStore } from '@/store/flow'
 import { treeFilter } from 'jetlinks-ui-components/es/Tree'
 import { generateRandomColor } from '../utils'
+import { cloneDeep } from 'lodash-es'
 
 type Emits = {
   (e: 'update:visible', data: boolean): void
@@ -182,7 +185,7 @@ watch(
 watch(
   () => props.variables,
   (val) => {
-    checkedKeys.value = val.map((m) => m.value)
+    checkedKeys.value = cloneDeep(val.map((m) => m.value))
   },
   { immediate: true, deep: true },
 )
