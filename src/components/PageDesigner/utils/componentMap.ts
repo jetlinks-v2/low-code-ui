b9910da50c9c7c64f5800f677ba3fee719f45084
XieYongHong <18010623010@163.com>
2023-11-23 20:05:25 +0800

    feat: 添加proTableLayout数据源配置
    * feat: 添加proTableLayout分页配置参数以及单项的render
    
    * feat: 添加proTableLayout数据源配置

--- src/components/PageDesigner/utils/componentMap.ts
+++ src/components/PageDesigner/utils/componentMap.ts
@@ -1,11 +1,9 @@
-import Search from '@LowCode/components/Search/Search.vue'
 import { Button, Tag, Text } from '../components/BaseComponent/index'
 
 const componentMap = {
     'text': Text,
     'button': Button,
     'tag': Tag,
-    'search': Search
 }
 
 export default componentMap;
