10913096584766f524a86420902510b0ed2b0e55
XieYongHong <18010623010@163.com>
2023-12-05 20:10:12 +0800

    * fix: pageDesign中Table添加render
    * fix: pageDesign中Table添加render

---
+++ src/components/PageDesigner/components/Panels/Config/components/Search/renderConfig.vue
@@ -0,0 +1,37 @@
+<template>
+  <j-button @click="visible = true">配置</j-button>
+  <ModalConfig
+    v-if="visible"
+    :value="value"
+    @save="save"
+    @cancel="cancel"
+  />
+</template>
+
+<script setup name="RenderConfig">
+import ModalConfig from './renderModal.vue'
+
+const props = defineProps({
+  value: {
+    type: String,
+    default: undefined
+  }
+})
+
+const emit = defineEmits(['update:value'])
+
+const visible = ref(false)
+
+const save = (v) => {
+  emit('update:value', v)
+}
+
+const cancel = () => {
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PageDesigner/components/Panels/Config/components/Search/renderModal.vue
@@ -0,0 +1,40 @@
+<template>
+  <j-modal
+    title="Render配置"
+    @cancel="cancel"
+    @ok="save"
+  >
+    <span>function render(record, route, router, h, utils){</span>
+    <ProMonaco
+      v-model:value="myValue"
+    />
+    <span>}</span>
+  </j-modal>
+</template>
+
+<script setup name="RenderModal">
+import {ProMonaco} from '../ProMonaco'
+
+const props = defineProps({
+  value: {
+    type: String,
+    default: ''
+  }
+})
+
+const emit = defineEmits(['update:value'])
+
+const myValue = ref(props.value)
+
+const save = () => {
+  emit('save', myValue.value)
+}
+
+const cancel = () => {
+  emit('cancel')
+}
+</script>
+
+<style scoped>
+
+</style>
