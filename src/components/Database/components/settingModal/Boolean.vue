970303e32e60a3ad48b46487614e096d6c9f0d3a
XieYongHong <18010623010@163.com>
2023-12-13 14:25:36 +0800

    fix: 优化crud中的规则配置
    * fix: 优化功能指令查询
    
    * fix: 优化crud中id配置
    
    * fix: 优化crud中的规则配置

---
+++ src/components/Database/components/settingModal/Boolean.vue
@@ -0,0 +1,18 @@
+<template>
+  <j-form-item label="数据操作" name="spec">
+    <Spec v-model:value="model.spec" />
+  </j-form-item>
+</template>
+
+<script setup name="CRUDSettingBoolean">
+import {inject} from "vue";
+import { SETTING_FORM_MODEL } from "@LowCode/components/Database/util";
+import Spec from './Spec.vue'
+
+const model = inject(SETTING_FORM_MODEL, {})
+
+</script>
+
+<style scoped>
+
+</style>
