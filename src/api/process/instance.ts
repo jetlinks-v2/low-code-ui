265af1299db55435a846d9783702fedb1c07dbb0
XieYongHong <18010623010@163.com>
2023-12-14 15:40:10 +0800

    fix: 合并dev

---
+++ src/api/process/instance.ts
@@ -0,0 +1,41 @@
+import { request } from '@jetlinks-web/core'
+
+/**
+ * 条件查询实例
+ * @returns 
+ */
+export const getList_api = (data: any) => request.post(`/process/deployment/detail/_query`,data);
+
+/**
+ * 条件查询所有实例
+ * @returns 
+ */
+export const getAllInstance_api = (data: any) => request.post(`/process/deployment/detail/_query/no-paging`, data);
+
+/**
+ * 复制为模型
+ */
+export const copy_api = (data: any) => request.post(`/process/deployment/${data.id}/_convert_to_definition`, data);
+
+/**
+ * 删除实例
+ */
+export const del_api = (id: string) => request.remove(`/process/deployment/${id}`)
+
+/**
+ * 修改实例状态
+ * @returns 
+ */
+export const updateState_api = (data: any) => request.put(`/process/deployment/${data.id}`, data);
+
+/**
+ * 查询对应流程实例可发起成员
+ * @returns 
+ */
+export const getMembers_api = (id: string) => request.get(`/process/deployment/${id}/allowed-members`);
+
+/**
+ * 全量修改流程实例可发起成员
+ * @returns 
+ */
+export const updateMembers_api = (id:string, data: any) => request.put(`/process/deployment/${id}/allowed-members`, data);
---
+++ src/api/process/monitor.ts
@@ -0,0 +1,73 @@
+import { request } from '@jetlinks-web/core'
+
+/**
+ * 查询流程监控
+ * @returns
+ */
+export const getList_api = (data: any) => {
+  const { history, params, ...rest } = data
+  return request.postParams(
+    `/process/runtime/processes/_query`,
+    {
+      ...rest,
+      ...params,
+    },
+    {
+      history,
+    },
+  )
+}
+
+/**
+ * 查询流程监控
+ * @returns
+ */
+export const getAllList_api = (data: any) => {
+  const { history, params, ...rest } = data
+  return request.postParams(
+    `/process/runtime/processes/_query/no-paging`,
+    {
+      ...rest,
+      ...params,
+    },
+    {
+      history,
+      distinctBy: 'creatorId'
+    },
+  )
+}
+
+/**
+ * 获取流程详情
+ * @returns
+ */
+export const getDetail_api = (id: string) =>
+  request.get(`/process/runtime/process/detail/${id}`)
+
+/**
+ * 关闭流程监控
+ * @returns
+ */
+export const close_api = (processId: string) =>
+  request.put(`/process/runtime/${processId}/_repeal`)
+
+/**
+ * 根据关联信息查询流程
+ * @param data 
+ * @returns 
+ */
+export const process_list = (data: any) => request.post(`/process/runtime/processes/withLink/_query`, data)
+
+/**
+ * 转交流程
+ * @param data 
+ * @returns 
+ */
+export const process_tran = (data: any) => request.post(`/process/runtime/_transfer`, data)
+
+/**
+ * 原办理人
+ * @param data 
+ * @returns 
+ */
+export const original_list = (data: any) => request.post(`/process/runtime/identity/_query/no-paging?distinctBy=dimensionKey`, data)
