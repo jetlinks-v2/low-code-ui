389a598c601b792f3edd3a5dc9c12931af13cbdb
100011797 <2642441182@qq.com>
2024-01-10 16:03:54 +0800

    fix: 21261

--- src/components/FormDesigner/components/Components/Editor.vue
+++ src/components/FormDesigner/components/Components/Editor.vue
@@ -19,6 +19,7 @@
 import {Editor, Toolbar} from '@wangeditor/editor-for-vue'
 import '@wangeditor/editor/dist/css/style.css' // 引入 css
 import {fileUpload} from '@LowCode/api/comm'
+import {cloneDeep} from "lodash-es";
 
 const props = defineProps({
   value: {
@@ -29,7 +30,6 @@ const props = defineProps({
     type: Number,
     default: 200
   },
-
   placeholder: {
     type: String,
     default: '请输入'
@@ -45,7 +45,8 @@ const valueHtml = ref('') // 内容 HTML
 const toolbarConfig = {
   excludeKeys: ['fullScreen', 'insertImage', 'codeBlock', 'redo', '|', 'group-video'] // insertLink
 }
-const editorConfig = {
+
+const init = {
   placeholder: props.placeholder,
   readOnly: props.disabled,
   MENU_CONF: {
@@ -61,6 +62,8 @@ const editorConfig = {
     }
   }
 }
+
+const editorConfig = ref(cloneDeep(init))
 const handleCreated = (editor: any) => {
   editorRef.value = editor // 记录 editor 实例，重要！
 }
@@ -77,6 +80,11 @@ const handleChange = (editor: any) => {
 
 watchEffect(() => {
   valueHtml.value = props.value || ''
+  editorConfig.value = {
+    ...cloneDeep(init),
+    readOnly: props.disabled,
+    placeholder: props.placeholder,
+  }
 })
 
 onBeforeUnmount(() => {// 组件销毁时，也及时销毁编辑
